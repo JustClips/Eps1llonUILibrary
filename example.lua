89f7deebbda0d301fbbfb7df4cf65febd6e25e87
JustClips <135924737+JustClips@users.noreply.github.com>
2025-07-22 14:04:19 -0500

    Update example.lua

--- example.lua
+++ example.lua
@@ -1,26 +1,24 @@
 local Eps1llonUI = require("src.Eps1llonUI")
 
-local win = Eps1llonUI:CreateWindow({title = "Executor Demo"})
+local win = Eps1llonUI:CreateWindow({title = "Eps1llon Hub"})
 
 win:AddLabel({text = "Welcome to Eps1llonUILibrary!"})
 
-win:AddButton({text = "Click Me", onClick = function()
-    print("Clicked!")
-    win:Notify({text = "You clicked the button!"})
-end})
+win:AddSlider({
+    text = "Volume",
+    min = 0,
+    max = 100,
+    default = 50,
+    onChange = function(val)
+        print("Slider:", val)
+    end
+})
 
-win:AddToggle({text = "Godmode", default = false, onToggle = function(state)
-    print("Godmode:", state)
-end})
-
-win:AddSlider({text = "Speed", min = 1, max = 100, default = 10, onChange = function(val)
-    print("Speed:", val)
-end})
-
-win:AddTextbox({placeholder = "Enter a command", onEnter = function(txt)
-    print("Command:", txt)
-end})
-
-win:AddDropdown({choices = {"Red", "Green", "Blue"}, default = "Green", onSelect = function(val)
-    print("Color picked:", val)
-end})
+win:AddDropdown({
+    text = "Theme",
+    choices = {"Blue", "Red", "Green"},
+    default = "Blue",
+    onSelect = function(val)
+        print("Theme selected:", val)
+    end
+})
